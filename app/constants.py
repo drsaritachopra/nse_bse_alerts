58f7ded9a6067d830af9d2e09d1c4fbf28fa6eb8
drsaritachopra <drsaritachopra@gmail.com>
2025-08-31 23:41:46 +0530

    Add files via upload

---
+++ app/constants.py
@@ -0,0 +1,11 @@
+# file: app/constants.py
+CA_KEYWORDS = [
+    "dividend",
+    "bonus",
+    "split",
+    "rights",
+    "buyback",
+    "merger",
+    "demerger",
+]
+# Greedy match patterns (lowercased matching)
